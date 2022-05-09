277179e9c40b35cc97f1505f25949d52dcda67a3
Suraj Badwaik <101567697+Suraj-Badwaik@users.noreply.github.com>
2022-05-09 09:52:59 +0530

    small change

--- index.js
+++ index.js
@@ -3,14 +3,33 @@
  document.getElementById("navbar").innerHTML = navbar()
 
 
- document.addEventListener("keydown", SearchCategory);
+//  document.addEventListener("keydown", SearchCategory);
  
- function SearchCategory(e) {
-    if (e.key == "Enter") {
-      let Keyword = document.querySelector("#search").value;
-      window.location.href="../../NORD_STROM_UNIT_3/women_Page/womes's_page.html"
-      let link = `https://www2.hm.com/en_in/women/shop-by-product/${Keyword}/_jcr_content/main/productlisting.display.json?sort=stock&image-size=small&image=model&offset=36&page-size=36`;
-      // document.querySelector("#Pickone").innerHTML = null;
-      ClothMap(link);
-    }
-  }
+//  function SearchCategory(e) {
+//     if (e.key == "Enter") {
+//       let Keyword = document.querySelector("#search").value;
+//       window.location.href="../../NORD_STROM_UNIT_3/women_Page/womes's_page.html"
+//       let link = `https://www2.hm.com/en_in/women/shop-by-product/${Keyword}/_jcr_content/main/productlisting.display.json?sort=stock&image-size=small&image=model&offset=36&page-size=36`;
+//       // document.querySelector("#Pickone").innerHTML = null;
+//       ClothMap(link);
+//     }
+//   }
+
+// import ClothMap from "./womenDataMap.js";
+
+//   document.addEventListener("keydown", SearchCategory);
+ 
+//   function SearchCategory(e) {
+//      if (e.key == "Enter") {
+//          let Keyword = document.querySelector("#search").value;
+//         if(Keyword== ""){
+//              return false;
+//         }
+//         else{
+//             let link = `https://www2.hm.com/en_in/women/shop-by-product/${Keyword}/_jcr_content/main/productlisting.display.json?sort=stock&image-size=small&image=model&offset=36&page-size=36`;
+//             window.location.href="./womes's_page.html"
+//             // document.querySelector("#Pickone").innerHTML = null;
+//             ClothMap(link);
+//          }
+//      }  
+//    }
